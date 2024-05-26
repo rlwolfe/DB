a4a41ed6952a1bc800a7cbfc7a111107d81f22c2
Rachel <67902191+rlwolfe@users.noreply.github.com>
2024-05-26 09:58:47 +0300

    Update Drop.sql to match syntax of postgresql

--- Drop.sql
+++ Drop.sql
@@ -7,38 +7,50 @@
 --
 
 --
+-- Drop Table    : 'Works'   
+--
+DROP TABLE if EXISTS "Works";
+
+--
 -- Drop Table    : 'Security'   
 --
-DROP TABLE Security;
+DROP TABLE if EXISTS "Security";
 
 --
 -- Drop Table    : 'Manager'   
 --
-DROP TABLE Manager;
+DROP TABLE if EXISTS "Manager";
 
 --
 -- Drop Table    : 'Secratary'   
 --
-DROP TABLE Secratary;
+DROP TABLE if EXISTS "Secratary";
 
 --
 -- Drop Table    : 'Preserver'   
 --
-DROP TABLE Preserver;
+DROP TABLE if EXISTS "Preserver";
 
 --
 -- Drop Table    : 'Librarian'   
 --
-DROP TABLE Librarian;
+DROP TABLE if EXISTS "Librarian";
 
 --
 -- Drop Table    : 'Shift'   
 --
-DROP TABLE Shift;
+DROP TABLE if EXISTS "Shift";
 
 --
 -- Drop Table    : 'Employee'   
 --
-DROP TABLE Employee;
+DROP TABLE if EXISTS "Employee";
+
+DROP TYPE IF EXISTS dept;
+DROP TYPE IF EXISTS team;
+DROP TYPE IF EXISTS days;
+DROP TYPE IF EXISTS genre;
+DROP TYPE IF EXISTS specialty;
+DROP TYPE IF EXISTS post;
 
 DROP SEQUENCE seq_Employee_EmpID;
