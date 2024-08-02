2d4764c8e923e368b3e7e626c1958dbd199d9fa9
Rachel <67902191+rlwolfe@users.noreply.github.com>
2024-08-02 15:24:49 +0300

    placed in dir

---
+++ Stage 3/select_dept_shift_emp_view.sql
@@ -0,0 +1,2 @@
+SELECT empid, name, day_of_week, timeslot
+	FROM public.all_employee_shifts_per_dept;
--- select_dept_shift_emp_view.sql
+++
@@ -1,2 +0,0 @@
-SELECT empid, name, day_of_week, timeslot
-	FROM public.all_employee_shifts_per_dept;
